29f4a35b7a47c0625e0f94cc194eba8851ff0139
Maksim Andreev <m@andreev.cx>
2017-12-19 12:08:23 +0300

    move script files & change some env in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# set vars
-WORK_DIR=$(cd ~ && pwd)
-
 # Prepare to deploy
 sudo apt update
 sudp apt install git -y
 
 # Deploy app
-cd $WORK_DIR
-if [ -d $WORK_DIR/reddit ]; then
-    rm -rf $WORK_DIR/reddit && git clone https://github.com/Otus-DevOps-2017-11/reddit.git; else
+cd $HOME
+if [ -d $HOME/reddit ]; then
+    rm -rf $HOME/reddit && git clone https://github.com/Otus-DevOps-2017-11/reddit.git; else
     git clone https://github.com/Otus-DevOps-2017-11/reddit.git
 fi
 
